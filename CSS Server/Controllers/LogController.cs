21f35e953a5d46e11b350e7cab8ec880388cc9a5
SietzeKoonstra <43609198+SietzeKoonstra@users.noreply.github.com>
2021-12-15 15:38:49 +0100

    loggerpage

---
+++ CSS Server/Controllers/LogController.cs
@@ -0,0 +1,19 @@
+﻿using CSS_Server.Models.Database.DBObjects;
+using CSS_Server.Models.Database.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CSS_Server.Controllers
+{
+    public class LogController : Controller
+    {
+        private static readonly SQLiteRepository<DBLog> _repository  = new SQLiteRepository<DBLog>();
+        public IActionResult Index()
+        {
+            List<DBLog> logs = _repository.GetAll();
+            logs.Reverse();
+
+            return View(logs);
+        }
+    }
+}
