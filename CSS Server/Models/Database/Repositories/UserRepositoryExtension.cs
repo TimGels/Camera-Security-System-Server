04e0215606869ea9226ec7337e09608f828e08ec
SietzeKoonstra <43609198+SietzeKoonstra@users.noreply.github.com>
2021-12-16 09:43:25 +0100

    Removed Email
    - Removed all email fields from controllers, model, viewmodals and views.
    - UserName is now unique.
    - added proper errorhandling when a non unique username is given.

--- CSS Server/Models/Database/Repositories/UserRepositoryExtension.cs
+++ CSS Server/Models/Database/Repositories/UserRepositoryExtension.cs
@@ -2,17 +2,16 @@
 using CSS_Server.Models.Database.DBObjects;
 using SQLite;
 using System.Linq;
-using System.Reflection;
 
 namespace CSS_Server.Models.Database.Repositories
 {
     public static class UserRepositoryExtension
     {
-        public static User GetByEmail(this SQLiteRepository<DBUser> userRepo, string email)
+        public static User GetByEmail(this SQLiteRepository<DBUser> userRepo, string userName)
         {
             using SQLiteConnection connection = DatabaseHandler.Instance.CreateConnection();
-            string sql = string.Format("select * from `{0}` where email = ?", userRepo.TableName);
-            DBUser dbUser = connection.Query<DBUser>(sql, email).FirstOrDefault();
+            string sql = string.Format("select * from `{0}` where userName = ?", userRepo.TableName);
+            DBUser dbUser = connection.Query<DBUser>(sql, userName).FirstOrDefault();
 
             return dbUser != null ? new User(dbUser) : null;
         }
--- CSS Server/ViewModels/LogInViewModel.cs
+++ CSS Server/ViewModels/LogInViewModel.cs
@@ -4,9 +4,8 @@ namespace CSS_Server.ViewModels
 {
     public class LogInViewModel
     {
-        [Required(ErrorMessage = "An email is required to login!")]
-        [EmailAddress(ErrorMessage = "What a weird email you have. We're not going to do anything with this.")]
-        public string Email { get; set; }
+        [Required(ErrorMessage = "An username is required to login!")]
+        public string UserName { get; set; }
 
         [Required(ErrorMessage = "A password is required to login!")]
         public string Password { get; set; }
